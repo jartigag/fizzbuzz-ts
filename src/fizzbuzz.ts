3376c8a67710a0a54a50cb2751e3ab2e3e0345cf
jartigag <jartigag@pm.me>
2021-10-16 11:21:36 +0200

    Just cosmetic changes

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,8 +1,8 @@
 export const fizzBuzzNumber = (num: number) => {
-    if (num<1) return Error('Invalid number')
+    if (num<1)     return new Error('Invalid number')
     if (num%3===0) return 'Fizz'
-    if (num%5===0) return 'Buzz';
-    return num;
+    if (num%5===0) return 'Buzz'
+    return num
 }
 
 export const fizzBuzzArray = (arrNum: Array<number>) => {
